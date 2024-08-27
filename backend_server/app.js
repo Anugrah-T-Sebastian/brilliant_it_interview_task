ae23c0d4b6e431075fac02393c87a37a4ee17430
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2024-08-27 19:48:54 +0100

    Request rate limiter added to backend server

--- backend_server/app.js
+++ backend_server/app.js
@@ -9,6 +9,12 @@ const documentRouter = require("./routes/document");
 const queryRouter = require("./routes/query");
 const addPineconeIndex = require("./middleware/PineconeClient");
 
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+  })
+);
 app.use(express.json());
 app.use(cors());
 app.use(addPineconeIndex);
